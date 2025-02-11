f760257008c0755de40e6d3bc4be1c4a5b8dcae6
bazzomir <bazzomir97@gmail.com>
2025-02-11 16:02:53 +0100

    fixed some syntax errors.

--- api/src/app.js
+++ api/src/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const superheroRouters = require('./routes/superheroRoutes');
+const superheroRoutes = require('./routes/superheroRoutes');
 
 const app = express();
 
-app.use(express.json())
-app.use('/api/superheroes', superheroRouters);
+app.use(express.json());
+app.use('/api/superheroes', superheroRoutes); 
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
--- api/src/controllers/superheroController.js
+++ api/src/controllers/superheroController.js
@@ -2,21 +2,21 @@ const SuperheroModel = require('../models/superheroModel');
 
 const SuperheroController = {
     createSuperhero: (req, res) => {
-        const {name, superpower, humilityScore} = req.body;
-
-        if(SuperheroModel.findByName(name)) {
+        const { name, superpower, humilityScore } = req.body;
+        
+        if (SuperheroModel.findByName(name)) {
             return res.status(409).json({
                 error: 'A superhero with this name already exists!'
             });
         }
-
-        const newHero = SuperheroModel.create({name, superpower, humilityScore});
+        
+        const newHero = SuperheroModel.create({ name, superpower, humilityScore });
         res.status(201).json(newHero);
     },
-
+    
     getAllSuperheroes: (req, res) => {
         const heroes = SuperheroModel.findAll();
-        req.json(heroes);
+        res.json(heroes);
     }
 };
 
