0e134b830b0e75c862e5e6c9b840c951a2077d64
bazzomir <bazzomir97@gmail.com>
2025-02-11 10:11:12 +0100

    added app.js && server.js

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,14 @@
-console.log("Hello Bazzo :D")
-
 const express = require('express');
+
 const app = express();
 
-app.listen(3030);
+app.use(express.json())
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({
+        error: 'Something went wrong!'
+    });
+});
+
+module.exports = app;
--- api/src/server.js
+++ api/src/server.js
@@ -1,1 +1,6 @@
-console.log("Hello Bazzo :D")
+const app = require('./app');
+const port = process.env.PORT || 3030;
+
+app.listen(port, () => {
+    // console.log(`Superhero API running on http://localhost:${port}`);
+});
