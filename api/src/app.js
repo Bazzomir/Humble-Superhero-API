5b2cb8ab5b69f4037624cbbc06dbe9bb44defc5e
bazzomir <bazzomir97@gmail.com>
2025-02-11 11:00:46 +0100

    added superheroRouters

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const superheroRouters = require('./routes/superheroRoutes');
 
 const app = express();
 
 app.use(express.json())
+app.use('/api/superheroes', superheroRouters);
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
