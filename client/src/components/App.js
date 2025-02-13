9aacb347fe42606594a6d06b52597a1ffdf6d9d4
bazzomir <bazzomir97@gmail.com>
2025-02-13 09:48:07 +0100

    added header, footer, style and responsive

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
 import '../assets/css/App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from './component/Footer';
+import Header from './component/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <Footer />
     </div>
   );
 }
