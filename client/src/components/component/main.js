132ee9b470283a33aca12995f1bec2c7977c7164
bazzomir <bazzomir97@gmail.com>
2025-02-15 03:37:17 +0100

    ...

--- client/src/components/component/main.js
+++ client/src/components/component/main.js
@@ -6,21 +6,28 @@ import { getSuperheroes, postSuperheroes } from '../../api/api';
 function Main() {
     const inputRef = useRef(null);
     const [superheroes, setSuperheroes] = useState([]);
+    const [showPostHero, setShowPostHero] = useState(false);
 
     const handleClick = () => {
         inputRef.current.classList.add("activeSearchBox");
     };
 
-    useEffect(() => {
-        loadSuperheroes();
-    })
+    const togglePostHero = () => {
+        setShowPostHero(!showPostHero);
+    };
+
+    const resetToDefault = () => {
+        setShowPostHero(false); 
+        inputRef.current.value = '';
+        inputRef.current.classList.remove("activeSearchBox");
+    };
 
     const loadSuperheroes = async () => {
         try {
             const data = await getSuperheroes();
             setSuperheroes(data);
         } catch (err) {
-            console.log('Error Loading Superheroes...', err)
+            console.log('Error Loading Superheroes...', err);
         }
     };
 
@@ -29,21 +36,27 @@ function Main() {
             await postSuperheroes(newHero);
             loadSuperheroes();
         } catch (err) {
-            console.log('Error adding superhero.', err)
+            console.log('Error adding superhero.', err);
         }
     };
+
+    useEffect(() => {
+        loadSuperheroes();
+    }, []);
 
     return (
         <div className="container mx-auto px-4 py-8">
             <div className="d-flex justify-content-around my-5">
-                <button className="btn btn-outline-orange w-25">List of Superheroes</button>
+                <button className="btn btn-outline-orange w-25" onClick={resetToDefault}>List of Superheroes</button>
                 <div className="search-box">
                     <button className="btn-search" onClick={handleClick}><i className="fa fa-search"></i></button>
                     <input type="text" ref={inputRef} className="input-search" placeholder="Type to Search..." />
                 </div>
-                <button className="btn btn-outline-orange w-25">Create a new Superhero</button>
+                <button className="btn btn-outline-orange w-25" onClick={togglePostHero}>Create a new Superhero</button>
             </div>
-            <PostHero onAdd={handleAddSuperhero} />
+            <div className={`post-hero-container ${showPostHero ? "show" : ""}`}>
+                {showPostHero && <PostHero onAdd={handleAddSuperhero} />}
+            </div>
             <GetHero superheroes={superheroes} />
         </div>
     );
